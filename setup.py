cde8e717318bdc7d509143876a86187e343bb683
Rui Catarino <55796280+ruitcatarino@users.noreply.github.com>
2024-10-05 11:09:47 +0100

    First release

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="typedispatch",
+    version="0.1.0",
+    author="Rui Catarino",
+    description="TypeDispatch is a Python utility for registering and dispatching functions based on object types, supporting inheritance through method resolution order (MRO).",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ruitcatarino/typedispatch",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+    ],
+)
