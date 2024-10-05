cf49f15d3f033981d41caf139107581b56b2ac77
Rui Catarino <55796280+ruitcatarino@users.noreply.github.com>
2024-10-05 14:57:33 +0100

    Refactor to version as an env variable

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
+import os
 from setuptools import setup, find_packages
+
+version = os.getenv("PACKAGE_VERSION", "0.0.1")
 
 setup(
     name="typedispatch",
-    version="0.3.0",
+    version=version,
     author="Rui Catarino",
+    description="TypeDispatch is a Python utility for registering and dispatching functions based on object types and predicates, supporting inheritance through method resolution order (MRO).",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/ruitcatarino/typedispatch",
     packages=find_packages(),
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
