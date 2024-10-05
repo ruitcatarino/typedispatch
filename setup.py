cbce401860ad53634a54f7952a972f9a05324dbc
Rui Catarino <55796280+ruitcatarino@users.noreply.github.com>
2024-10-05 11:25:58 +0100

    Bump Version => 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="typedispatch",
-    version="0.1.0",
+    version="0.2.0",
     author="Rui Catarino",
     description="TypeDispatch is a Python utility for registering and dispatching functions based on object types, supporting inheritance through method resolution order (MRO).",
     long_description=open("README.md", "r").read(),
